686729ccf5159b4c9998e0ece3f5cb12b76ebd47
lhincapie0 <lauhincapie97@gmail.com>
2021-02-14 00:15:15 -0500

    Added baseurl

--- src/app/dashboard/services/books/books.service.ts
+++ src/app/dashboard/services/books/books.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { IBook } from '../../interfaces/IBook';
 import { HttpClient } from '@angular/common/http';
+import {environment} from '../../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
+  baseUrl = environment.baseUrl;
   constructor(private http: HttpClient) { }
 
   getBooks(): Observable<IBook[]> {
-    return this.http.get<IBook[]>('http://localhost:8080/books');
+    return this.http.get<IBook[]>(this.baseUrl + 'books');
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://books-backend-lhincapie0.herokuapp.com/'
 };
 
 /*
