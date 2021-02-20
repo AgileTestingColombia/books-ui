c2edef4ff30ba90cdeee901eb6e713ff74097fd4
lhincapie0 <lauhincapie97@gmail.com>
2021-02-20 13:18:19 -0500

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -23,13 +24,6 @@ describe('AppComponent', () => {
   it(`should have as title 'books-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('books-ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('books-ui app is running!');
+    expect(app.title).toEqual('Library');
   });
 });
